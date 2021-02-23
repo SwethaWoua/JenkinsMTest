d95cb2dc2c4b45f5ec339768fd9ba076da09ec5b
SwethaWoua <78716947+SwethaWoua@users.noreply.github.com>
2021-02-23 17:27:15 +0530

    Update Div.java

--- src/main/java/mul_div/Div.java
+++ src/main/java/mul_div/Div.java
@@ -3,12 +3,8 @@ package mul_div;
 public class Div {
 
     public int div1(int a, int b){
-    String value = null;
-if(value.equals("VAL")){
-}
+
         return  a/b;
     }
-public void display(String content){
-System.out.println(content);
-}
+
 }
